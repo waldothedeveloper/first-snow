487a8f5ad93d85920292c4e914b66124dfaf921e
waldothedeveloper <w86compositor@gmail.com>
2018-09-19 19:44:05 -0400

    Finally got the drawer nav to open now to style it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
 import Navigation from './components/Navigation';
-import Drawer from './components/Drawer';
 
 library.add(faSnowflake);
 
@@ -21,7 +20,7 @@ class App extends Component {
 	render() {
 		return (
 			<MuiThemeProvider theme={theme}>
-				<Drawer />
+				<Navigation />
 			</MuiThemeProvider>
 		);
 	}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,131 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
 import IconButton from '@material-ui/core/IconButton';
+import Hidden from '@material-ui/core/Hidden';
+import Divider from '@material-ui/core/Divider';
 import MenuIcon from '@material-ui/icons/Menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const styles = {
+const drawerWidth = 240;
+
+const styles = (theme) => ({
 	root: {
-		flexGrow: 1
+		flexGrow: 1,
+		height: 440,
+		zIndex: 1,
+		overflow: 'hidden',
+		position: 'relative',
+		display: 'flex',
+		width: '100%'
+	},
+	appBar: {
+		position: 'absolute',
+		// marginLeft: drawerWidth,
+		[theme.breakpoints.up('md')]: {
+			width: '100%'
+		}
+	},
+	navIconHide: {
+		[theme.breakpoints.up('md')]: {
+			display: 'none'
+		}
+	},
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
+		width: drawerWidth,
+		[theme.breakpoints.up('md')]: {
+			position: 'relative'
+		}
+	},
+	content: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing.unit * 3
 	},
 	toRight: {
 		justifyContent: 'space-between'
 	}
-};
+});
 
-function Navigation(props) {
-	const { classes } = props;
+class Navigation extends React.Component {
+	state = {
+		mobileOpen: false
+	};
 
-	return (
-		<div className={classes.root}>
-			<AppBar position="static" color={props.bgColor}>
-				<Toolbar className={classes.toRight}>
-					<FontAwesomeIcon icon="snowflake" size="lg" spin />
-					<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-						<MenuIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+	handleDrawerToggle = () => {
+		this.setState((state) => ({ mobileOpen: !state.mobileOpen }));
+	};
+
+	render() {
+		const { classes } = this.props;
+
+		const drawer = (
+			<div>
+				<div className={classes.toolbar} />
+				<Divider />
+				<List>Local Weather</List>
+				<Divider />
+				<List>About</List>
+				<Divider />
+				<List>Contact</List>
+			</div>
+		);
+
+		return (
+			<div className={classes.root}>
+				<AppBar className={classes.appBar} position="static" color={classes.bgColor}>
+					<Toolbar className={classes.toRight}>
+						<FontAwesomeIcon icon="snowflake" size="lg" spin />
+						<IconButton
+							color="inherit"
+							aria-label="Open drawer"
+							onClick={this.handleDrawerToggle}
+							className={classes.navIconHide}
+						>
+							<MenuIcon />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<Hidden mdUp>
+					<Drawer
+						variant="temporary"
+						anchor="right"
+						open={this.state.mobileOpen}
+						onClose={this.handleDrawerToggle}
+						classes={{
+							paper: classes.drawerPaper
+						}}
+						ModalProps={{
+							keepMounted: true // Better open performance on mobile.
+						}}
+					>
+						{drawer}
+					</Drawer>
+				</Hidden>
+				<Hidden smDown implementation="css">
+					<Drawer
+						variant="permanent"
+						open
+						classes={{
+							paper: classes.drawerPaper
+						}}
+					>
+						{drawer}
+					</Drawer>
+				</Hidden>
+			</div>
+		);
+	}
 }
 
 Navigation.propTypes = {
-	classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Navigation);
+export default withStyles(styles, { withTheme: true })(Navigation);
