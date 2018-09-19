36676b8843380e65ae3ed49ae059801e30226174
Waldo the Developer <w86compositor@gmail.com>
2018-09-19 11:57:55 -0400

    Working on modifying the theme of material

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Navigation from './components/Navigation';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: '#2E3244',
+		secondary: '#fafbf6'
+	}
+});
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<p>TESTING</p>
+				<Navigation bgColor={theme.primary} />
 			</div>
 		);
 	}
